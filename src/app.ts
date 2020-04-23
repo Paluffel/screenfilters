ea4a895818f55fafb6446f5a22222997cc495e03
Paluffel <40839103+Paluffel@users.noreply.github.com>
2020-04-23 20:33:37 +0200

    Add files via upload

---
+++ src/app.ts
@@ -0,0 +1,59 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import {
+	Actor,
+	AnimationEaseCurves,
+	AnimationKeyframe,
+	AnimationWrapMode,
+	ButtonBehavior,
+	Collider,
+	ColliderGeometry,
+	CollisionLayer,
+	Context,
+	DegreesToRadians,
+	Quaternion,
+	TextAnchorLocation,
+	User,
+	Vector3
+} from '@microsoft/mixed-reality-extension-sdk';
+import * as MRE from '@microsoft/mixed-reality-extension-sdk';
+import * as MRESDK from '@microsoft/mixed-reality-extension-sdk';
+
+export default class HelloWorld {
+	public expectedResultDescription = "Different grabbable items.";
+
+	constructor(private context: Context, private baseUrl: string) {
+		this.context.onUserJoined((user) => this.userJoined(user));
+	}
+
+	// Create list to keep track of items attached to users.
+	private attachedItems: {[id: string]: Actor} = {};
+
+    private userJoined(user: User) {
+        // Code to run when a user joins.
+        console.log(`User joined: ${user.name}`);
+        console.log(user);
+        if (user.name === "AndrewVR,Paluffel"){
+            Actor.CreateFromLibrary(this.context, {
+                resourceId: "artifact: 1429917004155847128",
+                actor: {
+                    name: 'Invert',
+                    attachment: {
+                        userId: user.id,
+                        attachPoint: 'head'
+                    },
+                    transform: {local: {
+                        position: { x: 0, y: 0, z: 0.13 },
+                        scale: { x: 0.05, y: 0.05, z: 0.025},
+                        rotation: Quaternion.FromEulerAngles(270 * DegreesToRadians, 0 * DegreesToRadians, 0 * DegreesToRadians)
+                    }}
+                }
+            });
+        }
+    }
+}
+
+
