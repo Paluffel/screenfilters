5de64e294ff47eec34ca1b9cc8d8b866074214e3
Paluffel <40839103+Paluffel@users.noreply.github.com>
2020-04-23 20:46:36 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,7 @@ export default class HelloWorld {
         // Code to run when a user joins.
         console.log(`User joined: ${user.name}`);
         console.log(user);
-        if (user.name === "Paluffel"){
+        if (user.name === "Wouter L(G)BTQ+"){
             Actor.CreateFromLibrary(this.context, {
                 resourceId: "artifact: 1429917011101614557",
                 actor: {
