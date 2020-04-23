4c4377807078c9bf63c5a31697aef9c93a4d59fc
Paluffel <40839103+Paluffel@users.noreply.github.com>
2020-04-23 20:42:48 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -38,7 +38,7 @@ export default class HelloWorld {
         console.log(user);
         if (user.name === "Paluffel"){
             Actor.CreateFromLibrary(this.context, {
-                resourceId: "artifact: 1429917017258852831",
+                resourceId: "artifact: 1429917011101614557",
                 actor: {
                     name: 'Retro',
                     attachment: {
