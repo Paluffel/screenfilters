430ee11c9d236bea761069f592eeaba124f9b11e
Paluffel <40839103+Paluffel@users.noreply.github.com>
2020-04-24 20:18:22 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -65,7 +65,7 @@ export default class HelloWorld {
 			if (!this.attachedItems[user.id]) {
 				// If item for user does not exist, create it and add to list.
 				this.attachedItems[user.id] = Actor.CreateFromLibrary(this.context, {
-					resourceId: "artifact: 1429917004155847128",
+					resourceId: "artifact: 1429917011101614557",
 					actor: {
 						name: 'Invert',
 						grabbable: true,
