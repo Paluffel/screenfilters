9b1deed93dbd55d0d407b381b4b7f9ce54c99212
Paluffel <40839103+Paluffel@users.noreply.github.com>
2020-04-24 19:56:47 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,62 @@ import {
 	AnimationKeyframe,
 	AnimationWrapMode,
 	ButtonBehavior,
+	Collider,/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import {
+	Actor,
+	AnimationEaseCurves,
+	AnimationKeyframe,
+	AnimationWrapMode,
+	ButtonBehavior,
 	Collider,
+	ColliderGeometry,
+	CollisionLayer,
+	Context,
+	DegreesToRadians,
+	Quaternion,
+	TextAnchorLocation,
+	User,
+	Vector3
+} from '@microsoft/mixed-reality-extension-sdk';
+import * as MRE from '@microsoft/mixed-reality-extension-sdk';
+import * as MRESDK from '@microsoft/mixed-reality-extension-sdk';
+
+export default class HelloWorld {
+	public expectedResultDescription = "Different grabbable items.";
+
+	constructor(private context: Context, private baseUrl: string) {
+		this.context.onUserJoined((user) => this.userJoined(user));
+	}
+
+	// Create list to keep track of items attached to users.
+	private attachedItems: {[id: string]: Actor} = {};
+
+    private userJoined(user: User) {
+        // Code to run when a user joins.
+        console.log(`User joined: ${user.name}`);
+        console.log(user);
+        if (user.name === "Paluffel"){
+            Actor.CreateFromLibrary(this.context, {
+                resourceId: "artifact: 1455171633768563080",
+                actor: {
+                    name: 'Invert',
+                    grabbable: true,
+                    transform: {local: {
+                        position: { x: 0, y: 0, z: 0 },
+                        scale: { x: 1, y: 1, z: 1},
+                    }}
+                }
+            });
+        }
+    }
+}
+
+
+
 	ColliderGeometry,
 	CollisionLayer,
 	Context,
