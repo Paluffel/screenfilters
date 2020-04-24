c489f3d1f809cc529e099efdf87c2df0f54f71dc
Paluffel <40839103+Paluffel@users.noreply.github.com>
2020-04-24 18:39:06 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -46,8 +46,8 @@ export default class HelloWorld {
                         attachPoint: 'spine-middle'
                     },
                     transform: {local: {
-                        position: { x: 0, y: 0, z: 0 },
-                        scale: { x: 4, y: 4, z: 4},
+                        position: { x: 0, y: -1, z: 0 },
+                        scale: { x: 1, y: 1, z: 1},
                        
                     }}
                 }
